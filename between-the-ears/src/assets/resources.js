f6cfe7939647ed0f92e2cb9d1505a6e59d3877e5
Kelly Henderson <kellyjeanhenderson@gmail.com>
2023-06-20 23:27:31 -0500

    resource data

---
+++ between-the-ears/src/assets/resources.js
@@ -0,0 +1,154 @@
+let resources=[
+{
+    title:"Traveling on an Airplane Social Story",
+    category: "plane",
+    type:"social story",
+    link:"https://www.youtube.com/watch?v=MRtypPpfwAo"
+
+},
+{
+    title:"Flying Safely",
+    category:"plane",
+    type:"social story ",
+    link:"https://www.youtube.com/watch?v=dmjeCZ_GMS4"
+
+},
+{
+    title:"Autism Airplane Preparation: Model Me Airplane Social Skills Training",
+    category:"plane",
+    type:"social story",
+    link:"https://www.youtube.com/watch?v=w6gFae4QiGo"
+
+},
+{
+    title:"My First Time Flying on an Airplane! | Airplane SCience| SciShow Kids",
+    category:"plane",
+    type:"social story",
+    link:"https://www.youtube.com/watch?v=1DvFLiBmdTQ"
+
+},
+{
+    title:"Going on an Airplane Musical Social Story",
+    category:"plane",
+    type:"song",
+    link:"https://www.youtube.com/watch?v=eh7UreYehmk"
+
+},
+{
+    title:"Maisy Goes on a Plane",
+    category:"plane",
+    type:"book",
+    link:"https://www.youtube.com/watch?v=_-cXmRoKCxA"
+
+},
+{
+    title:"My First Airplane Ride",
+    category:"plane",
+    type:"book",
+    link:"https://www.youtube.com/watch?v=ln11VBGPPkI"
+
+},
+{
+    title:"Going to a Restaurant/Autism Social Skills Video",
+    category:"restaurant",
+    type:"social story",
+    link:"https://www.youtube.com/watch?v=pOUI1rr7QDk"
+
+},
+{
+    title:"Staying at a Hotel/Autism Social Skills Video",
+    category:"hotel",
+    type:"social story",
+    link:"hhttps://www.youtube.com/watch?v=pWY9bN2hqHs"
+
+},
+{
+    title:"Going to Disney - Social Story",
+    category:"disney",
+    type:"song",
+    link:"https://www.youtube.com/watch?v=qXo1EBdM0OY"
+
+},
+{
+    title:"A Different Way Is Okay",
+    category:"change",
+    type:"song",
+    link:"https://www.youtube.com/watch?v=IhCvebNFPek"
+
+},
+{
+    title:"Daniel Tiger's Neighborhood | The fireworks are Scary| PBS kids",
+    category:"noise",
+    type:"social story",
+    link:"https://www.youtube.com/watch?v=3QBqcpkok30"
+
+},
+{
+    title:"When Things Get Too LOUD by Anne Alcott",
+    category:"noise",
+    type:"book",
+    link:"https://www.youtube.com/watch?v=gGEhTk62yMM"
+
+},
+{
+    title:"No Matter Where You Go, You Can Find Something That You Know ",
+    category:"change",
+    type:"song",
+    link:"https://www.youtube.com/watch?v=OYUixVl3Kcc"
+
+},
+{
+    title:"When We Do Something New Let's Talk About What We'll Do",
+    category:"change",
+    type:"song",
+    link:"https://www.youtube.com/watch?v=I6wofOMbq0M"
+
+},
+{
+    title:"Social Story: Trip to DisneyLand",
+    category:"disney",
+    type:"social story",
+    link:"https://www.youtube.com/watch?v=Fo-7xgdo9fs"
+
+},
+{
+    title:"Changing My Schedule/ Autism Social Skills Video ",
+    category:"change",
+    type:"social story",
+    link:"https://www.youtube.com/watch?v=awAmJcLOXA4"
+
+},
+{
+    title:"Wait Your Turn",
+    category:"waiting",
+    type:"song",
+    link:"https://youtu.be/gnZPBaWhiqo"
+
+},
+{
+    title:"The Waiting Song",
+    category:"waiting",
+    type:"song",
+    link:"https://www.youtube.com/watch?v=qJ8B8JWrYzc"
+
+},
+{
+    title:"Waiting Is Not Forever by Elizabeth Verdick",
+    category:"waiting",
+    type:"book",
+    link:"https://youtu.be/S9Mnd12-lBs"
+
+},
+{
+    title:"Waiting Is Not Easy!",
+    category:"waiting",
+    type:"book",
+    link:"https://youtu.be/PPswJFlmQp0"
+
+}
+
+
+
+]
+
+export default resources
